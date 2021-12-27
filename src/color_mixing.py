280f70a291ade9b2fde79477d5707bd7dcd310fc
AlphaBeta906 <63622656+AlphaBeta906@users.noreply.github.com>
2021-12-27 14:39:16 +0800

    CodeQL said to remove this
    Yeah

--- src/color_mixing.py
+++ src/color_mixing.py
@@ -37,6 +37,5 @@ api.add_resource(ColorMixing, '/color_mixing/<string:color>')
 if __name__ == '__main__':
     app.run(
         host='localhost',
-        port=5000,
-        debug=True
+        port=5000
     )
