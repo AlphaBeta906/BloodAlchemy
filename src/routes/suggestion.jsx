18f14a4555876ad96d09dda31977f9c3ec7c026c
AlphaBeta906 <dragonkingalyx@gmail.com>
2022-01-02 13:35:23 +0800

    OHHHHHH BOIIIII

--- src/routes/suggestion.jsx
+++ src/routes/suggestion.jsx
@@ -10,10 +10,6 @@ import Error from "./error";
 export default function Suggestion() {
     let params = useParams();
 
-    const e1 = params.suggestion.split("=")[0].split("+")[0];
-    const e2 = params.suggestion.split("=")[0].split("+")[1];
-    const reaction = params.suggestion.split("=")[1];
-
     const { user } = useContext(UserContext);
     const [result, setResult] = useState("");
     const [output, setOutput] = useState("");
@@ -44,74 +40,110 @@ export default function Suggestion() {
                 set(ref(db, 'suggestions/' + params.suggestion + '/votes'), snapshot1.val()[params.suggestion].votes + 1);
 
                 if (snapshot1.val()[params.suggestion].votes === 1) {
-                    set(ref(db, 'reactions/' + e1 + "+" + e2), reaction);
+                    const reaction = params.suggestion.split("=")[1];
+                    set(ref(db, 'reactions/' + params.suggestion.split("=")[0]), reaction);
 
-                    get(ref(db, 'elements/')).then((snapshot2) => {
-                        if (snapshot2.val()[reaction] === undefined) {
-                            var d = new Date();
-                            let text = d.toUTCString();
+                    if (params.suggestion.includes("Refine(") || params.suggestion.includes("Ferment(")) {
+                        const process = params.suggestion.split("=")[0].replace(")", "").split("(")[0];
+                        const elem = params.suggestion.split("=")[0].replace(")", "").split("(")[1];
 
-                            var generation = 0
+                        get(ref(db, 'elements/')).then((snapshot2) => {
+                            if (snapshot2.val()[reaction] === undefined) {
+                                var d = new Date();
+                                var text = d.toUTCString();
+                                var process_color = ""
 
-                            get(ref(db, 'elements/')).then((snapshot) => {
-                                if (snapshot.val()[e1].generation < snapshot.val()[e2]["generation"]) {
-                                    generation = snapshot.val()[e2]["generation"] + 1;
-                                } else if (snapshot.val()[e1]["generation"] > snapshot.val()[e2]["generation"]) {
-                                    generation = snapshot.val()[e1]["generation"] + 1;
-                                } else if (snapshot.val()[e1]["generation"] === snapshot.val()[e2]["generation"]) {
-                                    generation = snapshot.val()[e1]["generation"] + 1;
+                                switch (process) {
+                                    case "Refine":
+                                        process_color = "EEEEEE";
+                                        break;
+                                    default:
+                                        process_color = "4F2956";
+                                        break;
                                 }
 
-                                var complexity1 = 0
-                                if (e1 === e2) {
-                                    complexity1 = snapshot.val()[e1]["complexity"];
-                                } else {
-                                    if (snapshot.val()[e1]["complexity"] > snapshot.val()[e2]["complexity"]) {
-                                        complexity1 = snapshot.val()[e1]["complexity"] + 1;
-                                    } else if (snapshot.val()[e1]["complexity"] < snapshot.val()[e2]["complexity"]) {
-                                        complexity1 = snapshot.val()[e2]["complexity"] + 1;
-                                    } else if (snapshot.val()[e1]["complexity"] === snapshot.val()[e2]["complexity"]) {
-                                        complexity1 = snapshot.val()[e1]["complexity"] + 1;
-                                    }
-                                }
-
-                                mix_hex(snapshot.val()[e1]["color"], snapshot.val()[e2]["color"]).then((hex) => {
-                                    var color = hex;
-
+                                mix_hex(snapshot2.val()[elem].color, process_color).then((res) => {
                                     set(ref(db, 'elements/' + reaction), {
-                                        "color": color,
-                                        "generation": generation,
-                                        "complexity": complexity1,
+                                        "color": res,
+                                        "generation": snapshot2.val()[elem]["generation"] + 1,
+                                        "complexity": snapshot2.val()[elem]["complexity"] + 1,
                                         "date": text,
                                         "creator": snapshot1.val()[params.suggestion].creator
                                     });
-
-                                    setResult(
-                                        <>
-                                            Added to database with new element: <Link to={'/info/' + reaction}>{reaction}</Link>
-                                        </>
-                                    )
-                                }).catch((err) => {
-                                    setResult(err.toString());
-                                    return
                                 });
-                            }).catch((error) => {
-                                setResult("Error: " + error.toString());
-                            });
-                        } else {
-                            setResult("Added to database!");
-                        }
+                            }
+                        }).catch((err) => {
+                            setResult(err);
+                        });
+                    } else {
+                        get(ref(db, 'elements/')).then((snapshot2) => {
+                            var e1 = params.suggestion.split("=")[0].split("+")[0];
+                            var e2 = params.suggestion.split("=")[0].split("+")[1];
 
-                        get(ref(db, `users/${user}/inventory/${reaction}`)).then((snapshot) => {
-                            set(ref(db, `users/${user}/inventory/${reaction}`), snapshot.val() + 1);
+                            if (snapshot2.val()[reaction] === undefined) {
+                                var d = new Date();
+                                let text = d.toUTCString();
+
+                                var generation = 0
+
+                                get(ref(db, 'elements/')).then((snapshot) => {
+                                    if (snapshot.val()[e1].generation < snapshot.val()[e2]["generation"]) {
+                                        generation = snapshot.val()[e2]["generation"] + 1;
+                                    } else if (snapshot.val()[e1]["generation"] > snapshot.val()[e2]["generation"]) {
+                                        generation = snapshot.val()[e1]["generation"] + 1;
+                                    } else if (snapshot.val()[e1]["generation"] === snapshot.val()[e2]["generation"]) {
+                                        generation = snapshot.val()[e1]["generation"] + 1;
+                                    }
+
+                                    var complexity1 = 0
+                                    if (e1 === e2) {
+                                        complexity1 = snapshot.val()[e1]["complexity"];
+                                    } else {
+                                        if (snapshot.val()[e1]["complexity"] > snapshot.val()[e2]["complexity"]) {
+                                            complexity1 = snapshot.val()[e1]["complexity"] + 1;
+                                        } else if (snapshot.val()[e1]["complexity"] < snapshot.val()[e2]["complexity"]) {
+                                            complexity1 = snapshot.val()[e2]["complexity"] + 1;
+                                        } else if (snapshot.val()[e1]["complexity"] === snapshot.val()[e2]["complexity"]) {
+                                            complexity1 = snapshot.val()[e1]["complexity"] + 1;
+                                        }
+                                    }
+
+                                    mix_hex(snapshot.val()[e1]["color"], snapshot.val()[e2]["color"]).then((hex) => {
+                                        var color = hex;
+
+                                        set(ref(db, 'elements/' + reaction), {
+                                            "color": color,
+                                            "generation": generation,
+                                            "complexity": complexity1,
+                                            "date": text,
+                                            "creator": snapshot1.val()[params.suggestion].creator
+                                        });
+
+                                        setResult(
+                                            <>
+                                                Added to database with new element: <Link to={'/info/' + reaction}>{reaction}</Link>
+                                            </>
+                                        )
+                                    }).catch((err) => {
+                                        setResult(err.toString());
+                                        return
+                                    });
+                                }).catch((error) => {
+                                    setResult("Error: " + error.toString());
+                                });
+                            } else {
+                                setResult("Added to database!");
+                            }
+
+                            get(ref(db, `users/${user}/inventory/${reaction}`)).then((snapshot) => {
+                                set(ref(db, `users/${user}/inventory/${reaction}`), snapshot.val() + 1);
+                            }).catch((error) => {
+                                console.error(error);
+                            });
                         }).catch((error) => {
                             console.error(error);
                         });
-
-
-                    }).catch((error) => {
-                        console.error(error);
-                    });
+                    }
                 } else {
                     setResult("Voted!");
                 }
@@ -144,7 +176,7 @@ export default function Suggestion() {
             setOutput(
                 <div>
                     <center>
-                        <h2>Suggestion: {e1} + {e2} = {reaction}</h2>
+                        <h2>Suggestion: {params.suggestion}</h2>
                     </center>
 
                     <p style={{color: "#6ac4dc"}}>🗳 Votes: 2</p>
@@ -159,7 +191,7 @@ export default function Suggestion() {
             setOutput(
                 <div>
                     <center>
-                        <h2>Suggestion: {e1} + {e2} = {reaction}</h2>
+                        <h2>Suggestion: {params.suggestion}</h2>
                     </center>
 
                     <p style={{color: "#6ac4dc"}}>🗳 Votes: {snapshot.val()[params.suggestion].votes}</p>
