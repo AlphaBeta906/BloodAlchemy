e7430d8832555c43e35bdff1a2a85006c61c3099
AlphaBeta906 <dragonkingalyx@gmail.com>
2022-01-08 13:25:39 +0800

    bug2

--- src/javascript/tips.jsx
+++ src/javascript/tips.jsx
@@ -1,15 +1,23 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { useLocation } from "react-router-dom";
 import { randomInt } from "./random";
+import UserContext from "./userContext";
 
 export default function Tips() {
     const [tip, setTip] = useState("");
+    const { user, setUser } = useContext(UserContext);
     let location = useLocation();
 
     useEffect(() => {
         const tips = ["Stuck? Try randomly combining elements.", "Need more resources, mine it!", "In debt? Sell your elements!", "Need more watts? Upgrade your generators!", "You want to rise from slave to master? Invade a mine!"];
 
         setTip(tips[randomInt(tips.length)]);
+
+        if (user === "") {
+            setUser(localStorage.getItem("user"));
+        } else {
+            localStorage.setItem("user", user);
+        }
     }, [location.pathname])
 
     return (
