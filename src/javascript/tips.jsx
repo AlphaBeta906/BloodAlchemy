eca4ea66029662601c34a50fbd4635b4cf2d476e
AlphaBeta906 <dragonkingalyx@gmail.com>
2022-01-08 13:31:56 +0800

    SO EPIC MAN I DID IT (HORRAY)

--- src/javascript/tips.jsx
+++ src/javascript/tips.jsx
@@ -14,11 +14,14 @@ export default function Tips() {
         setTip(tips[randomInt(tips.length)]);
 
         if (user === "") {
-            setUser(localStorage.getItem("user"));
+            if (window.localStorage.getItem("user") !== null) {
+                setUser(window.localStorage.getItem("user"));
+                console.log(window.localStorage.getItem("user"));
+            }
         } else {
-            localStorage.setItem("user", user);
+            window.localStorage.setItem("user", user);
         }
-    }, [location.pathname])
+    }, [location.pathname, user, setUser]);
 
     return (
         <div>
