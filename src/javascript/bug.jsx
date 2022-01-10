5b7cafc56c1cc8915acd836173c4e1d5c9607879
AlphaBeta906 <dragonkingalyx@gmail.com>
2022-01-10 09:45:02 +0800

    brub

--- src/javascript/bug.jsx
+++ src/javascript/bug.jsx
@@ -3,16 +3,23 @@ export default function Bug() {
         <div>
             <center>
                 <h1>Bug</h1>
+            </center>
 
-                If you want to report a bug, please do the following:
-                <ul>
-                    <li>Go to <a href="https://github.com/AlphaBeta906/BloodAlchemy">GitHub</a> and create a new issue.</li>
-                    <li>Make the title the summary of the issue.</li>
-                    <li>In the body, please discuss in full detail about the issue, with verifiable evidence, that is clear.</li>
-                    <li>Attach a screenshot of the bug.</li>
-                    <li>Then submit it.</li>
-                </ul>
+            If you want to report a bug, please do the following:
+            <ul>
+                <li>Go to <a href="https://github.com/AlphaBeta906/BloodAlchemy">GitHub</a> and create a new issue.</li>
+                <li>Make the title the summary of the issue.</li>
+                <li>In the body, please discuss in full detail about the issue, with verifiable evidence, that is clear.</li>
+                <li>Attach a screenshot of the bug.</li>
+                <li>Then submit it.</li>
+            </ul>
 
+            By the way, here is a list of known bugs and issues (fixed but not much people updated, and unfixed):
+            <ul>
+                <li>Signing out doesn't work</li>
+            </ul>
+
+            <center>
                 <p>Thank you for being a beta-tester!</p>
             </center>
         </div>
