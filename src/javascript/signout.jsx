5be1bc21f52c4704517f689bcdb6a6f1f5829710
AlphaBeta906 <dragonkingalyx@gmail.com>
2022-01-10 19:11:53 +0800

    bug

--- src/javascript/signout.jsx
+++ src/javascript/signout.jsx
@@ -3,23 +3,22 @@ import { Navigate } from "react-router-dom";
 import UserContext from "./userContext";
 
 export default function Signout() {
-    const [output, setOutput] = useState("");
+    const [result, setResult] = useState("");
     const { setUser } = useContext(UserContext);
 
     const handleClick = () => {
         setUser("");
         localStorage.removeItem("user");
-        setOutput(
+        setResult(
             <Navigate to="/" />
         )
     };
 
-    setOutput(<div>
+    return (<div>
         <center>
             <h1>Sign Out</h1>
             <button onClick={handleClick}>Sign-Out</button>
         </center>
+        {result}
     </div>);
-
-    return output;
 }
