635ae578f22d430501e76d7261784c6c25c61bc3
AlphaBeta906 <dragonkingalyx@gmail.com>
2022-01-13 19:10:38 +0800

    epic

--- src/javascript/suggestion.jsx
+++ src/javascript/suggestion.jsx
@@ -121,7 +121,12 @@ export default function Suggestion() {
                                         setResult(err.toString());
                                     });
                                 }).catch((error) => {
-                                    setResult("Error: " + error.toString());
+                                    setResult(
+                                        <div class="bg-red-100 border-t border-b border-red-500 text-red-700 px-4 py-3 w-64" role="alert">
+                                            <p class="font-bold">🛑 Error 🛑</p>
+                                            <p class="text-sm">{error.toString()}</p>
+                                        </div>
+                                    );
                                 });
                             } else {
                                 setResult("Added to database!");
@@ -143,7 +148,12 @@ export default function Suggestion() {
                 setResult("This reaction has not been suggested yet.");
             }
         }).catch((error) => {
-            setResult(error.toString());
+            setResult(
+                <div class="bg-red-100 border-t border-b border-red-500 text-red-700 px-4 py-3 w-64" role="alert">
+                    <p class="font-bold">🛑 Error 🛑</p>
+                    <p class="text-sm">{error.toString()}</p>
+                </div>
+            );
         });
     }
 
