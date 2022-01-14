171460aa39bc05411d43ad4beb7792c20d126a47
ItzCountryballs <itzcountryballs@gmail.com>
2022-01-14 17:23:24 -0600

    Updated contact page

--- src/javascript/contact.jsx
+++ src/javascript/contact.jsx
@@ -2,7 +2,14 @@ export default function Contact() {
     return (
         <div>
             <center><p class="text-2xl">Contact</p></center>
-            This is the contact page.
+            <center>This is the contact page.</center>
+            <center><p class="text-2xl">ALPHABETA906</p></center>
+            <center>GitHub: AlphaBeta906</center>
+            <center>Discord: AlphaBeta906Alt#6315</center>
+            <center><p class="text-2xl">ITZCOUNTRYBALLS</p></center>
+            <center>GitHub: ItzCountryballs</center>
+            <center>Discord: ItzCountryballs#3682</center>
+            <center>Twitter: @ICountryballs</center>
         </div>
     );
 }
