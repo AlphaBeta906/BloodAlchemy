83169f8d1acbe7e8425d7f70297471e3d16fc37a
AlphaBeta906 <dragonkingalyx@gmail.com>
2022-01-14 18:53:30 +0800

    stuff

---
+++ src/javascript/faq.jsx
@@ -0,0 +1,24 @@
+export default function FAQ() {
+    return (
+        <div>
+            <center>
+                <p class="text-2xl">FAQ</p><br /><br />
+                
+                <p class="text-xl">What is this 'Blood Alchemy'?</p>
+                Blood Alchemy is a game where you can create elements and combine them to create new ones. Originally this was a Discord bot, but I decided to make it a website instead.<br /><br />
+
+                <p class="text-xl">I heard this game before...</p>
+                Yes, this is a game based on another game (Elemental 3) based on Doodle God.<br /><br />
+
+                <p class="text-xl">COPPA?</p>
+                No, we are legally outside of COPPA's juristriction, and we hate it so very much. We pledge to not add cookies nor trackers to the game.<br /><br />
+
+                <p class="text-xl">I saw a bad word in this site! Please remove it!</p>
+                We are not responsible for any bad words in the game, since this a community driven project. I am afraid we can't do anything about it.<br /><br />
+
+                <p class="text-xl">I want to talk to your manager!</p>
+                Take a chill pill. Go to the contact page, idiot.<br /><br />
+            </center>
+        </div>
+    )
+}
