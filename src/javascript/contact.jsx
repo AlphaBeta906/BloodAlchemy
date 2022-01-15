904545f3902888d2cc7a35e98cd4a54f689bf063
AlphaBeta906 <dragonkingalyx@gmail.com>
2022-01-15 08:17:40 +0800

    AAAAAAA

--- src/javascript/contact.jsx
+++ src/javascript/contact.jsx
@@ -2,11 +2,13 @@ export default function Contact() {
     return (
         <div>
             <center><p class="text-2xl">Contact</p></center>
-            <center>This is the contact page.</center>
-            <center><p class="text-2xl">ALPHABETA906</p></center>
+            <center>This is the contact page.</center><br />
+
+            <center><p class="text-xl">AlphaBeta906</p></center>
             <center>GitHub: AlphaBeta906</center>
-            <center>Discord: AlphaBeta906Alt#6315</center>
-            <center><p class="text-2xl">ITZCOUNTRYBALLS</p></center>
+            <center>Discord: AlphaBeta906Alt#6315</center><br />
+
+            <center><p class="text2xl">ItzCountryballs</p></center>
             <center>GitHub: ItzCountryballs</center>
             <center>Discord: ItzCountryballs#3682</center>
             <center>Twitter: @ICountryballs</center>
