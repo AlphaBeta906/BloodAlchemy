2dca384065945d38aecfc0d1ebdb77b33b6ce069
AlphaBeta906 <dragonkingalyx@gmail.com>
2022-01-16 19:25:29 +0800

    epic

--- src/javascript/test.jsx
+++ src/javascript/test.jsx
@@ -1,35 +1,36 @@
-import { useEffect } from "react"
+import { useEffect, useContext } from "react"
 import UserContext from "./userContext";
-import { useContext } from "react";
+import Error from "./error";
+
 export default function Test() {
     const { user } = useContext(UserContext);
 
-    if (["AlphaBeta906", "ItzCountryballs"].includes(user)) {
-        useEffect(() => {        
-            const pushAd = () => {
-                try {
-                    const adsbygoogle = window.adsbygoogle
-                    console.log({ adsbygoogle })
-                    adsbygoogle.push({})
-                } catch (e) {
-                    console.error(e.toString())
-                }
+    useEffect(() => {        
+        const pushAd = () => {
+            try {
+                const adsbygoogle = window.adsbygoogle
+                console.log({ adsbygoogle })
+                adsbygoogle.push({})
+            } catch (e) {
+                console.error(e.toString())
             }
+        }
 
-            let interval = setInterval(() => {
-                // Check if Adsense script is loaded every 300ms
-                if (window.adsbygoogle) {
-                    pushAd()
-                    // clear the interval once the ad is pushed so that function isn't called indefinitely
-                    clearInterval(interval)
-                }
-            }, 300)
-
-            return () => {
+        let interval = setInterval(() => {
+            // Check if Adsense script is loaded every 300ms
+            if (window.adsbygoogle) {
+                pushAd()
+                // clear the interval once the ad is pushed so that function isn't called indefinitely
                 clearInterval(interval)
             }
-        }, []);
+        }, 300)
 
+        return () => {
+            clearInterval(interval)
+        }
+    }, []);
+
+    if (["AlphaBeta906", "ItzCountryballs"].includes(user)) {
         return (
             <ins className='adsbygoogle'
             style={{ display: 'block' }}
