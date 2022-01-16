13f4d63b0ef57f3fac93bde6c8c5773ea41e02f5
AlphaBeta906 <dragonkingalyx@gmail.com>
2022-01-16 19:09:24 +0800

    v1.20.8 (final)

--- src/javascript/test.jsx
+++ src/javascript/test.jsx
@@ -1,44 +1,45 @@
 import { useEffect } from "react"
 import UserContext from "./userContext";
 import { useContext } from "react";
-if (["AlphaBeta906", "ItzCountryballs"].includes(user)) {
 export default function Test() {
     const { user } = useContext(UserContext);
-    useEffect(() => {        
-        const pushAd = () => {
-            try {
-                const adsbygoogle = window.adsbygoogle
-                console.log({ adsbygoogle })
-                adsbygoogle.push({})
-            } catch (e) {
-                console.error(e.toString())
-            }
-        }
 
-        let interval = setInterval(() => {
-            // Check if Adsense script is loaded every 300ms
-            if (window.adsbygoogle) {
-                pushAd()
-                // clear the interval once the ad is pushed so that function isn't called indefinitely
+    if (["AlphaBeta906", "ItzCountryballs"].includes(user)) {
+        useEffect(() => {        
+            const pushAd = () => {
+                try {
+                    const adsbygoogle = window.adsbygoogle
+                    console.log({ adsbygoogle })
+                    adsbygoogle.push({})
+                } catch (e) {
+                    console.error(e.toString())
+                }
+            }
+
+            let interval = setInterval(() => {
+                // Check if Adsense script is loaded every 300ms
+                if (window.adsbygoogle) {
+                    pushAd()
+                    // clear the interval once the ad is pushed so that function isn't called indefinitely
+                    clearInterval(interval)
+                }
+            }, 300)
+
+            return () => {
                 clearInterval(interval)
             }
-        }, 300)
+        }, []);
 
-        return () => {
-            clearInterval(interval)
-        }
-    }, []);
-
-    return (
-        <ins className='adsbygoogle'
-        style={{ display: 'block' }}
-        data-ad-client='ca-pub-8343050917784042'
-        data-ad-slot='4420988986'
-        data-ad-format='auto' />
-    );
- }
-} else {
-    return (
-        <Error status="403" />
-    )
+        return (
+            <ins className='adsbygoogle'
+            style={{ display: 'block' }}
+            data-ad-client='ca-pub-8343050917784042'
+            data-ad-slot='4420988986'
+            data-ad-format='auto' />
+        );
+    } else {
+        return (
+            <Error status="404" />
+        )
+    }
 }
