1c4be9f765314ac24e8ccd4019952016c63c4bdf
AlphaBeta906 <dragonkingalyx@gmail.com>
2022-01-16 08:18:45 +0800

    epic

--- src/javascript/bug.jsx
+++ src/javascript/bug.jsx
@@ -5,19 +5,16 @@ export default function Bug() {
                 <p class="text-2xl">Bug</p>
             </center>
 
-            If you want to report a bug, please do the following:
-            <ul class="list-disc">
-                <li>Go to <a href="https://github.com/AlphaBeta906/BloodAlchemy">GitHub</a> and create a new issue.</li>
-                <li>Make the title the summary of the issue.</li>
-                <li>In the body, please discuss in full detail about the issue, with verifiable evidence, that is clear.</li>
-                <li>Attach a screenshot of the bug.</li>
-                <li>Then submit it.</li>
-            </ul>
+            If you want to report a bug, please do the following:<br /><br />
+            
+            1. Go to <a href="https://github.com/AlphaBeta906/BloodAlchemy">GitHub</a> and create a new issue.<br />
+            2. Make the title the summary of the issue.<br />
+            3. In the body, please discuss in full detail about the issue, with verifiable evidence, that is clear.<br />
+            4. Attach a screenshot of the bug.<br />
+            5. Then submit it.<br /><br />
 
-            By the way, here is a list of known bugs and issues (fixed but not much people updated, and unfixed):
-            <ul class="list-disc">
-                <li>Signing out doesn't work</li>
-            </ul>
+            By the way, here is a list of known bugs and issues (fixed but not much people updated, and unfixed):<br />
+            <center>There are no current bugs!</center>
 
             <center>
                 <p>Thank you for being a beta-tester!</p>
--- src/javascript/updatelog.jsx
+++ src/javascript/updatelog.jsx
@@ -4,12 +4,10 @@ export default function UpdateLog() {
             <center><p class="text-2xl">Current Version: 1.20.7</p></center>
             <center>What came along with the version?</center>
             
-            <ul class="list-disc">
-                <li>A contact page</li>
-                <li>A new and improved menu page</li>
-                <li>A new home page</li>
-                <li>Split with the About page, with the FAQ page</li>
-            </ul>
+            - A contact page.<br/>
+            - A new menu page.<br/>
+            - A new home page.<br/>
+            - A split with the About page, with the FAQ page.<br/>
         </div>
     );
 }
