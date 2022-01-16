751d8be616e690e4bf394acb12937d915bebed6d
ItzCountryballs <itzcountryballs@gmail.com>
2022-01-16 07:38:18 -0600

    V.1.20.9 (Variant 1.1.1)

--- src/javascript/updatelog.jsx
+++ src/javascript/updatelog.jsx
@@ -1,7 +1,7 @@
 export default function UpdateLog() {
     return (
         <div>
-            <center><p class="text-2xl">Current Version: 1.20.8</p></center>
+            <center><p class="text-2xl">Current Version: 1.20.9</p></center>
             <center>What came along with the version?</center>
             - A special message for specific humans who did some "stuff"...<br />
             - Test page has been locked to administrators to hide new features.
