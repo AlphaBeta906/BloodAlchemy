a7c2859f2105ec64560569b1b45491621e5d1288
AlphaBeta906 <dragonkingalyx@gmail.com>
2022-01-16 08:01:54 +0800

    epic

--- src/javascript/updatelog.jsx
+++ src/javascript/updatelog.jsx
@@ -1,11 +1,15 @@
 export default function UpdateLog() {
     return (
         <div>
-            <center><p class="text-2xl">Current Version: 1.19.7</p></center>
+            <center><p class="text-2xl">Current Version: 1.20.7</p></center>
             <center>What came along with the version?</center>
-            <center>A new feature has been added for administrators of Blood Alchemy on the Home page.</center>
-            <center>This page was created.</center>
-            <center>Contact page now has the administrators' contact medias.</center>
+            
+            <ul class="list-disc">
+                <li>A contact page</li>
+                <li>A new and improved menu page</li>
+                <li>A new home page</li>
+                <li>Split with the About page, with the FAQ page</li>
+            </ul>
         </div>
     );
 }
