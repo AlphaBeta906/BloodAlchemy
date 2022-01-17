1b411d46cf5a24c1115fefec79fb91ca70bf0c63
AlphaBeta906 <dragonkingalyx@gmail.com>
2022-01-17 08:01:54 +0800

    itz fix

--- src/javascript/updatelog.jsx
+++ src/javascript/updatelog.jsx
@@ -4,9 +4,9 @@ export default function UpdateLog() {
             <center><p class="text-2xl">Current Version: 1.20.9</p></center>
             <center>What came along with the version?</center>
             - Grammar fixes on some pages.<br />
-            - Admin ranks has been added to the inventory page.
-            - Hints are now added.
-            - New changes to the Blood Alchemy logo.
+            - Admin ranks has been added to the inventory page.<br/>
+            - Hints are now added.<br/>
+            - New changes to the Blood Alchemy logo.<br/>
             - Second password field has been named to Confirm Password to avoid any confusions.
         </div>
     );
