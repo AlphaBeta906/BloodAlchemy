e731495e8fd8f7cf2ad3084c3d96edaaea77f03d
AlphaBeta906 <dragonkingalyx@gmail.com>
2022-01-18 19:28:34 +0800

    epic

---
+++ src/javascript/firebase.jsx
@@ -0,0 +1,14 @@
+/* test 1, 2, 3. Good */
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCobYaVDIC1wA_vQo_9-AWOOiMRVn7wkTw",
+    authDomain: "elementals4.firebaseapp.com",
+    databaseURL: "https://elementals4-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "elementals4",
+    storageBucket: "elementals4.appspot.com",
+    messagingSenderId: "493819791208",
+    appId: "1:493819791208:web:00ecc6ec14821d1514f8dd",
+    measurementId: "G-VHXF7NPGV0"
+};
+
+export default firebaseConfig;
