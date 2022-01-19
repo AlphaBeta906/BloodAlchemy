6a33d09706a2ca3e1c738524d37d96e9d388a267
AlphaBeta906 <dragonkingalyx@gmail.com>
2022-01-19 08:25:44 +0800

    o

--- src/javascript/ranks.jsx
+++ src/javascript/ranks.jsx
@@ -9,9 +9,9 @@ export default function Ranks() {
 
       <center><span class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-gray-600 bg-gray-200">⚙️ Administrator</span></center>
       <center>The administrator rank is for Blood Alchemy developers and staff. Any users with this rank would manage the site, answer what problem you are having right now, and fix bugs.</center>
-      <center>Obtainable by: Being a staff or developer at Blood Alchemy.</center>
+      <center>Obtainable by: Being a staff or developer at Blood Alchemy.</center><br /><br />
 
-      <center><span class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-gray-600 bg-gray-200">🧪 Trusted BETA Tester</span></center>
+      <center><span class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-green-600 bg-green-200">🧪 Trusted BETA Tester</span></center>
       <center>The trusted BETA tester rank is for BETA testers which Blood Alchemy developers deem trustworthy. Any users with this rank would test new BETA features that hasn't been released to the public yet.</center>
       <center>Obtainable by: When a staff of Blood Alchemy deems you trustworthy of testing new features.</center>
 
