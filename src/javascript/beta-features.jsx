50b5ea2d91fe1653b7659c402c0da9858cc8ce7c
AlphaBeta906 <dragonkingalyx@gmail.com>
2022-01-21 13:13:37 +0800

    o

--- src/javascript/beta-features.jsx
+++ src/javascript/beta-features.jsx
@@ -176,11 +176,8 @@ export default function BETAFeatures() {
                       <label for="toggleB" class="flex items-center cursor-pointer">
                         <div class="relative">
                           <input type="checkbox" id="toggleB" class="sr-only" />
-                          <div class="line block bg-white w-14 h-8 rounded-full"></div>
+                          <div class="line block bg-gray-500 w-14 h-8 rounded-full"></div>
                           <div class="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
-                        </div>
-                        <div class="ml-3 text-gray-700 font-medium">
-                          Toggle Me!
                         </div>
                       </label>
                     </div>
