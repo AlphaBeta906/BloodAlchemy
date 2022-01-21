6d9d28dca17f4d458588ddd0ce0c09402397bfa8
ItzCountryballs <93321936+ItzCountryballs@users.noreply.github.com>
2022-01-21 09:00:52 +0800

    Administrator Panel for V.1.20.9.3

---
+++ src/javascript/admin-panel.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react"
+import UserContext from "./userContext";
+import Error from "./error";
+
+export default function AdminPanel() {
+    const { user } = useContext(UserContext);
+
+    if (["AlphaBeta906", "ItzCountryballs"].includes(user)) {
+  return (
+    <div>
+      <center>
+        <p class="text-2xl">Administrator Panel</p><br /><br />
+      </center>
+
+      <center>No features yet, Administrators must add something to the panel.</center>
+    </div>
+  );
+ } else {
+  return (
+   <Error status="404" />
+  )
+ }
+}
